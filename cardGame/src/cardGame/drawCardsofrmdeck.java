032d5a40fe03d37ba808229a741716a65b12e581
nishchal awale <nishchal awale>
2016-11-11 19:31:04 -0600

    this is the outline of the code

---
+++ cardGame/src/cardGame/drawCardsofrmdeck.java
@@ -0,0 +1,20 @@
+package cardGame;
+
+public class drawCardsofrmdeck {
+
+	public static void main(String[] args) {
+		// initializing array for suits and the rank of cards and array for deck of cards
+		//initializing the deck of cards
+		//suffling the deck of cards
+		//generating a index for a random number from 1 to 52
+		//print the cards that has been withdrawed from the deck
+		//initialize ace jack qween and king 
+		//assign values for Ace jack qween and king
+		//add the cards together and show output
+		
+		
+		
+
+	}
+
+}
